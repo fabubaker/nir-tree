d6d42658ffe612fcd4a8f7dc9e39d656f639b692
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2023-02-28 13:04:29 -0500

    Modify nyc script to keep coordinates with zeroes

--- scripts/nyc_taxi_preprocess.sh
+++ scripts/nyc_taxi_preprocess.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
+set -x
 
 # Use this script to pre-process the raw NYC Taxi dataset obtained from UCR Star.
 # It generates a final csv file with only the pickup and dropoff points.
 
 USAGE='nyc_taxi_preprocess.sh <NYCTaxi.csv>'
 
-cat $1 | cut -d',' -f1,2,13,14 | sort -u | sed '/^0.0,/d' | sed '/,0.0$/d' > $1_cleaned.csv
-cat $1_cleaned.csv | cut -d',' -f1,2 > $1_pickup.csv
-cat $1_cleaned.csv | cut -d',' -f3,4 > $1_dropoff.csv
+filename=(basename $1 .csv)
 
-cat $1_pickup.csv $1_dropoff.csv > $1_combined.csv
+# Extract points and unique them.
+cat $1 | cut -d',' -f1,2,13,14 | sort -u > ${filename}_cleaned.csv
+
+# Separate pickup and dropoff points and then append them together.
+cat ${filename}_cleaned.csv | cut -d',' -f1,2 > ${filename}_pickup.csv
+cat ${filename}_cleaned.csv | cut -d',' -f3,4 > ${filename}_dropoff.csv
+cat ${filename}_pickup.csv ${filename}_dropoff.csv > ${filename}_combined.csv
 
