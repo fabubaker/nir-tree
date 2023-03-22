94d145bfbd4d6ef24440c49f5d93725614a341a2
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2023-03-22 14:19:36 -0400

    Add UL to buffer pool params

--- src/include/globals/globals.h
+++ src/include/globals/globals.h
@@ -7,10 +7,10 @@
 
 const unsigned dimensions = DIM;
 // Buffer pool memory for bulk-loading.
-#define GEN_TREE_BUFFER_POOL_MEMORY 40960 * 130000
+#define GEN_TREE_BUFFER_POOL_MEMORY 40960UL * 130000UL
 
 // Buffer pool memory for running main benchmarks.
-#define MAIN_BUFFER_POOL_MEMORY 4096 * 13000
+#define MAIN_BUFFER_POOL_MEMORY 4096UL * 13000UL
 
 // The two above are separate since you might wanna benchmark
 // both with different buffer pool sizes.
