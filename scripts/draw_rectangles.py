9fcf80b483ee35d902f5a62e0262e6199abe8e37
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2023-04-30 13:17:25 -0400

    Draw rectangles with alpha=0.2 and disable gridlines

--- scripts/draw_rectangles.py
+++ scripts/draw_rectangles.py
@@ -26,13 +26,13 @@ def draw_rectangles(rectangles):
         x_max, y_max = rectangle[1]
         width = x_max - x_min
         height = y_max - y_min
-        rect = patches.Rectangle((x_min, y_min), width, height, linewidth=1, edgecolor='black', facecolor='none')
+        rect = patches.Rectangle((x_min, y_min), width, height, linewidth=1, edgecolor='black', facecolor='blue', alpha=0.2)
         ax.add_patch(rect)
 
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.title('Rectangles')
-    plt.grid(True)
+    plt.grid(visible=False)
     plt.show()
 
 input_file = '/home/fabubaker/Desktop/printed_tree.txt'
