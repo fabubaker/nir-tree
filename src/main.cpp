dbd1f99e98180edb7b7c0e7c6d5a2085e3256be5
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2023-09-29 15:15:32 -0400

    Add -R option to getopt

--- src/main.cpp
+++ src/main.cpp
@@ -130,7 +130,7 @@ int main(int argc, char *argv[]) {
   std::map<std::string, double> configD;
   std::map<std::string, std::string> configS;
 
-  while ((option = getopt(argc, argv, "t:m:a:b:n:s:r:v:z:g:p:B:P:S:f:L:A:D:")) != -1) {
+  while ((option = getopt(argc, argv, "t:m:a:b:n:s:r:v:z:g:p:B:P:S:f:L:A:D:R:")) != -1) {
     switch (option) {
       case 't': // Tree
       {
