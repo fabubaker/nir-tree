f1ae8766fc7f1b5ebcb3c62e2c8410b00fbb8dfc
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2023-10-12 11:30:41 -0400

    Replace rplustreedisk.h with rstartreedisk code

--- src/include/rplustreedisk/rplustreedisk.h
+++ src/include/rplustreedisk/rplustreedisk.h
@@ -15,175 +15,186 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-namespace rplustreedisk
-{
-  template <int min_branch_factor, int max_branch_factor> class RPlusTreeDisk: public Index
-	{
-		public:
-			tree_node_handle root_;
-      tree_node_allocator node_allocator_;
-      std::string backing_file_;
+namespace rplustreedisk {
+    template <int min_branch_factor, int max_branch_factor>
+    class RPlusTreeDisk : public Index {
+    public:
+        static constexpr float p = 0.3; // For reinsertion entries. 0.3 by default
 
-      // Not required, but we mimic R* so file formats are the same.
-      std::vector<bool> hasReinsertedOnLevel;
+        tree_node_handle root;
+        std::unique_ptr<tree_node_allocator> node_allocator_;
+        std::string backing_file_;
 
-			// Constructors and destructors
-			RPlusTreeDisk(size_t memory_budget, const std::string &backing_file):
-        node_allocator_(memory_budget, backing_file), backing_file_(backing_file)
-      {
-        node_allocator_.initialize();
+        std::vector<bool> hasReinsertedOnLevel;
 
-        size_t existing_page_count = node_allocator_.buffer_pool_.get_preexisting_page_count();
+        // Constructors and destructors
+        RPlusTreeDisk(size_t memory_budget, std::string backing_file) : backing_file_(backing_file) {
+          node_allocator_ = std::make_unique<tree_node_allocator>(memory_budget, backing_file);
 
-        // If existing page count is zero, then create a new
-        // root node.
-        if (existing_page_count == 0) {
-            auto alloc_data = node_allocator_.create_new_tree_node<Node<min_branch_factor,max_branch_factor>>();
-            root_ = alloc_data.second;
+          // Initialize buffer pool
+          node_allocator_->initialize();
 
-            new (&(*(alloc_data.first))) Node<min_branch_factor,max_branch_factor>(
-              this, root_, tree_node_handle(nullptr)
-            );
+          hasReinsertedOnLevel = {false};
 
+          /* We need to figure out if there was already data, and read
+                       * that into memory if we have it. */
+          size_t existing_page_count = node_allocator_->buffer_pool_.get_preexisting_page_count();
+
+          // If this is a fresh tree, then make a fresh root
+          if (existing_page_count == 0) {
+            auto node_type = NodeHandleType(LEAF_NODE);
+            auto alloc = node_allocator_->create_new_tree_node<LeafNode<min_branch_factor, max_branch_factor>>(node_type);
+            root = alloc.second;
+            new (&(*(alloc.first))) LeafNode<min_branch_factor, max_branch_factor>();
             return;
+          }
+
+          std::string meta_file = backing_file_ + ".meta";
+          int fd = open(meta_file.c_str(), O_RDONLY);
+          assert(fd >= 0);
+
+          int rc = read(fd, (char *)&root, sizeof(root));
+          assert(rc == sizeof(root));
         }
 
-        // Find existing root node.
-        std::string meta_file = backing_file_ + ".meta";
-        int fd = open( meta_file.c_str(), O_RDONLY );
-        assert( fd >= 0 );
+        ~RPlusTreeDisk(){};
 
-        int rc = read( fd, (char *) &root_, sizeof( root_ ) );
-        assert( rc == sizeof( root_ ) );
-      }
+        // Datastructure interface
+        std::vector<Point> exhaustiveSearch(Point requestedPoint);
+        std::vector<Point> search(Point requestedPoint);
+        std::vector<Point> search(Rectangle requestedRectangle);
+        void insert(Point givenPoint);
+        void remove(Point givenPoint);
 
-			~RPlusTreeDisk();
+        // Miscellaneous
+        unsigned checksum();
+        void print();
+        bool validate();
+        void stat();
+        void visualize();
 
-			// Datastructure interface
-			std::vector<Point> exhaustiveSearch(Point requestedPoint);
-			std::vector<Point> search(Point requestedPoint);
-			std::vector<Point> search(Rectangle requestedRectangle);
-			void insert(Point givenPoint);
-			void remove(Point givenPoint);
-
-			// Miscellaneous
-			unsigned checksum();
-			bool validate();
-			void stat();
-			void print();
-			void visualize();
-
-      inline pinned_node_ptr<Node<min_branch_factor,max_branch_factor>> get_node(tree_node_handle node_handle) {
-          auto ptr =
-              node_allocator_.get_tree_node<Node<min_branch_factor,max_branch_factor>>(node_handle);
-
-          ptr->treeRef = this;
+        inline pinned_node_ptr<LeafNode<min_branch_factor, max_branch_factor>> get_leaf_node(tree_node_handle node_handle) {
+          assert(node_handle.get_type() == LEAF_NODE);
+          auto ptr = node_allocator_->get_tree_node<LeafNode<min_branch_factor, max_branch_factor>>(node_handle);
           return ptr;
+        }
+
+        inline pinned_node_ptr<BranchNode<min_branch_factor, max_branch_factor>> get_branch_node(tree_node_handle node_handle) {
+          assert(node_handle.get_type() == BRANCH_NODE);
+          auto ptr = node_allocator_->get_tree_node<BranchNode<min_branch_factor, max_branch_factor>>(node_handle);
+          return ptr;
+        }
+
+        void write_metadata() {
+          // Step 1:
+          // Writeback everything to disk
+          node_allocator_->buffer_pool_.writeback_all_pages();
+
+          // Step 2:
+          // Write metadata file
+          std::string meta_fname = node_allocator_->get_backing_file_name() + ".meta";
+
+          int fd = open(meta_fname.c_str(), O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+          assert(fd >= 0);
+          // yes, yes, i should loop this whatever
+          int rc = write(fd, (char *)&root, sizeof(root));
+          assert(rc == sizeof(root));
+          close(fd);
+        }
+    };
+
+    template <int min_branch_factor, int max_branch_factor>
+    std::vector<Point> RPlusTreeDisk<min_branch_factor, max_branch_factor>::exhaustiveSearch(Point requestedPoint) {
+      std::vector<Point> v;
+      if (root.get_type() == LEAF_NODE) {
+        auto root_ptr = get_leaf_node(root);
+        root_ptr->exhaustiveSearch(requestedPoint, v);
+      } else {
+        auto root_ptr = get_branch_node(root);
+        root_ptr->exhaustiveSearch(requestedPoint, v);
       }
 
-      void write_metadata() override {
-        // Step 1:
-        // Writeback everything to disk
-        node_allocator_.buffer_pool_.writeback_all_pages();
+      return v;
+    }
 
-        // Step 2:
-        // Write metadata file
+    template <int min_branch_factor, int max_branch_factor>
+    std::vector<Point> RPlusTreeDisk<min_branch_factor, max_branch_factor>::search(Point requestedPoint) {
+      return point_search(root, requestedPoint, this);
+    }
 
-        auto root_node = get_node( root_ );
-        assert( root_node->self_handle_ == root_ );
-        std::string meta_fname = backing_file_ + ".meta";
-        int fd = open( meta_fname.c_str(), O_WRONLY |
-                O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR );
-        assert( fd >= 0 );
-        // yes, yes, i should loop this whatever
-        int rc = write( fd, (char *) &root_, sizeof(root_) );
-        assert( rc == sizeof(root_) );
-        close( fd );
+    template <int min_branch_factor, int max_branch_factor>
+    std::vector<Point> RPlusTreeDisk<min_branch_factor, max_branch_factor>::search(Rectangle
+                                                                                   requestedRectangle) {
+      return rectangle_search(root, requestedRectangle, this);
+    }
+
+    template <int min_branch_factor, int max_branch_factor>
+    void RPlusTreeDisk<min_branch_factor, max_branch_factor>::insert(Point givenPoint) {
+      if (root.get_type() == LEAF_NODE) {
+        auto root_ptr = get_leaf_node(root);
+        std::fill(hasReinsertedOnLevel.begin(), hasReinsertedOnLevel.end(), false);
+        root = root_ptr->insert(this, this->root, givenPoint, hasReinsertedOnLevel);
+        return;
       }
-	};
+      auto root_ptr = get_branch_node(root);
+      std::fill(hasReinsertedOnLevel.begin(), hasReinsertedOnLevel.end(), false);
+      root = root_ptr->insert(this, this->root, givenPoint, hasReinsertedOnLevel);
+    }
 
-  template <int min_branch_factor, int max_branch_factor>
-  RPlusTreeDisk<min_branch_factor, max_branch_factor>::~RPlusTreeDisk() {
-    auto root_node = get_node( root_ );
-    root_node->deleteSubtrees();
+    template <int min_branch_factor, int max_branch_factor>
+    void RPlusTreeDisk<min_branch_factor, max_branch_factor>::remove(Point givenPoint) {
+      std::fill(hasReinsertedOnLevel.begin(), hasReinsertedOnLevel.end(), false);
+      if (root.get_type() == LEAF_NODE) {
+        auto root_ptr = get_leaf_node(root);
+        root = root_ptr->remove(givenPoint, hasReinsertedOnLevel);
+      } else {
+        auto root_ptr = get_branch_node(root);
+        root = root_ptr->remove(givenPoint, hasReinsertedOnLevel);
+      }
+    }
 
-    // FIXME GC root;
-  }
+    template <int min_branch_factor, int max_branch_factor>
+    unsigned RPlusTreeDisk<min_branch_factor, max_branch_factor>::checksum() {
+      if (root.get_type() == LEAF_NODE) {
+        auto root_ptr = get_leaf_node(root);
+        return root_ptr->checksum();
+      }
+      auto root_ptr = get_branch_node(root);
+      return root_ptr->checksum();
+    }
 
-  template <int min_branch_factor, int max_branch_factor>
-  std::vector<Point> RPlusTreeDisk<min_branch_factor, max_branch_factor>::exhaustiveSearch(
-          Point requestedPoint
-  ) {
-    std::vector<Point> v;
-    auto root_node = get_node( root_ );
-    root_node->exhaustiveSearch( requestedPoint, v );
+    template <int min_branch_factor, int max_branch_factor>
+    void RPlusTreeDisk<min_branch_factor, max_branch_factor>::print() {
+      std::ofstream outputFile("printed_rstar_tree.txt");
 
-    return v;
-  }
+      struct Printer {
+          Printer(std::ofstream &printFile): printFile(printFile) {}
 
-  template <int min_branch_factor, int max_branch_factor>
-  std::vector<Point> RPlusTreeDisk<min_branch_factor, max_branch_factor>::search(
-          Point requestedPoint
-  ) {
-    auto root_node = get_node( root_ );
-    return root_node->search( requestedPoint );
-  }
+          void operator()(RPlusTreeDisk<min_branch_factor, max_branch_factor> *treeRef, tree_node_handle node_handle) {
+            printPackedNodes<min_branch_factor, max_branch_factor>(treeRef, node_handle, printFile);
+          }
 
-  template <int min_branch_factor, int max_branch_factor>
-  std::vector<Point> RPlusTreeDisk<min_branch_factor, max_branch_factor>::search(
-          Rectangle requestedRectangle
-  ) {
-    auto root_node = get_node( root_ );
-    return root_node->search( requestedRectangle );
-  }
+          std::ofstream &printFile;
+      };
 
-  template <int min_branch_factor, int max_branch_factor>
-  void RPlusTreeDisk<min_branch_factor, max_branch_factor>::insert(
-          Point givenPoint
-  ) {
-    auto root_node = get_node( root_ );
-    root_ = root_node->insert( givenPoint );
-  }
+      Printer printer(outputFile);
+      treeWalker<min_branch_factor, max_branch_factor>(this, root, printer);
 
-  template <int min_branch_factor, int max_branch_factor>
-  void RPlusTreeDisk<min_branch_factor, max_branch_factor>::remove(
-          Point givenPoint
-  ) {
-    auto root_node = get_node( root_ );
-    root_ = root_node->remove( givenPoint );
-  }
+      outputFile.close();
+    }
 
-  template <int min_branch_factor, int max_branch_factor>
-  unsigned RPlusTreeDisk<min_branch_factor, max_branch_factor>::checksum()
-  {
-    auto root_node = get_node( root_ );
-    return root_node->checksum();
-  }
+    template <int min_branch_factor, int max_branch_factor>
+    bool RPlusTreeDisk<min_branch_factor, max_branch_factor>::validate() {
+      return true;
+    }
 
-  template <int min_branch_factor, int max_branch_factor>
-  bool RPlusTreeDisk<min_branch_factor, max_branch_factor>::validate()
-  {
-    return true;
-  }
+    template <int min_branch_factor, int max_branch_factor>
+    void RPlusTreeDisk<min_branch_factor, max_branch_factor>::stat() {
+      stat_node(root, this);
+    }
 
-  template <int min_branch_factor, int max_branch_factor>
-  void RPlusTreeDisk<min_branch_factor, max_branch_factor>::stat()
-  {
-    auto root_node = get_node( root_ );
-    root_node->stat();
-  }
+    template <int min_branch_factor, int max_branch_factor>
+    void RPlusTreeDisk<min_branch_factor, max_branch_factor>::visualize() {
 
-  template <int min_branch_factor, int max_branch_factor>
-  void RPlusTreeDisk<min_branch_factor, max_branch_factor>::print()
-  {
-    auto root_node = get_node( root_ );
-    root_node->printTree();
-  }
-
-  template <int min_branch_factor, int max_branch_factor>
-  void RPlusTreeDisk<min_branch_factor, max_branch_factor>::visualize()
-  {
-    //BMPPrinter p( 1000, 1000 );
-    //p.printToBMP( root_node );
-  }
-}
+    }
+} // namespace rstartreedisk
