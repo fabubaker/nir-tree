2b8e08063b08c406ad9835f70880f33a32ac8ed9
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2023-11-01 19:14:00 -0400

    Modify gen_rect to save to rects directory

--- gen_rect.sh
+++ gen_rect.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #Usage: ./gen_rect.sh tweets
-datasetname="$1"
+dataset_path="$1"
 
-python3 scripts/knn_script.py /ssd1/data/$datasetname.csv 10 1000 ${datasetname}_rect_10.tst
-python3 scripts/knn_script.py /ssd1/data/$datasetname.csv 100 1000 ${datasetname}_rect_100.tst
-python3 scripts/knn_script.py /ssd1/data/$datasetname.csv 1000 1000 ${datasetname}_rect_1000.tst
-python3 scripts/knn_script.py /ssd1/data/$datasetname.csv 10000 1000 ${datasetname}_rect_10000.tst
+dataset_name=$(echo $dataset_path | awk -F/ '{gsub(/\..*$/,"",$NF); print $NF}')
 
+mkdir -p rects
 
-
-
-
+python3 scripts/knn_script.py $dataset_path 10 1000 rects/${dataset_name}_rect_10.tst
+python3 scripts/knn_script.py $dataset_path 100 1000 rects/${dataset_name}_rect_100.tst
+python3 scripts/knn_script.py $dataset_path 1000 1000 rects/${dataset_name}_rect_1000.tst
+python3 scripts/knn_script.py $dataset_path 10000 1000 rects/${dataset_name}_rect_10000.tst
