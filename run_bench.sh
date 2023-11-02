6d2f921c295e53471e8886cf9d531637ae180fb7
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2023-11-02 15:04:16 -0400

    Modify run_bench to use ./main from parent directory

--- run_bench.sh
+++ run_bench.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Usage:
-# ./run_bench.sh 1 data/tweets.csv exp
+# ./run_bench.sh 1 data/tweets.csv rects/ exp
 # Runs benchmarks
 
 tree_type="$1"
-dataset_path="$2"
-tag="$3"
+dataset_path=$(realpath $2)
+rects_dir=$(realpath $3)
+tag="$4"
 
 tree_name=''
 if [ $tree_type -eq 1 ]; then
@@ -23,17 +24,17 @@ buffer_mem=8000
 dataset_name=$(echo $dataset_path | awk -F/ '{gsub(/\..*$/,"",$NF); print $NF}')
 root_folder="${dataset_name}_${tag}_dir"
 
-./bin/main -t $tree_type -i $dataset_path -B $buffer_mem -S 1 \
-| tee $root_folder/${tree_name}_point_search.ot
+../bin/main -t $tree_type -i $dataset_path -B $buffer_mem -S 1 \
+| tee ${tree_name}_point_search.ot
 
-./bin/main -t $tree_type -i $dataset_path -B $buffer_mem -S 0 -R ./rects/${dataset_name}_rect_10.tst \
-| tee $root_folder/${tree_name}_rec_search_10.ot
+../bin/main -t $tree_type -i $dataset_path -B $buffer_mem -S 0 -R ${rects_dir}/${dataset_name}_rect_10.tst \
+| tee ${tree_name}_rec_search_10.ot
 
-./bin/main -t $tree_type -i $dataset_path -B $buffer_mem -S 0 -R ./rects/${dataset_name}_rect_100.tst \
-| tee $root_folder/${tree_name}_rec_search_100.ot
+../bin/main -t $tree_type -i $dataset_path -B $buffer_mem -S 0 -R ${rects_dir}/${dataset_name}_rect_100.tst \
+| tee ${tree_name}_rec_search_100.ot
 
-./bin/main -t $tree_type -i $dataset_path -B $buffer_mem -S 0 -R ./rects/${dataset_name}_rect_1000.tst \
-| tee $root_folder/${tree_name}_rec_search_1000.ot
+../bin/main -t $tree_type -i $dataset_path -B $buffer_mem -S 0 -R ${rects_dir}/${dataset_name}_rect_1000.tst \
+| tee ${tree_name}_rec_search_1000.ot
 
-./bin/main -t $tree_type -i $dataset_path -B $buffer_mem -S 0 -R ./rects/${dataset_name}_rect_10000.tst \
-| tee $root_folder/${tree_name}_rec_search_10000.ot
+../bin/main -t $tree_type -i $dataset_path -B $buffer_mem -S 0 -R ${rects_dir}/${dataset_name}_rect_10000.tst \
+| tee ${tree_name}_rec_search_10000.ot
