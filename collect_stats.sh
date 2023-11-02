10b61ce66e4765946d54a21ed375072a3098151f
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2023-11-02 14:16:10 -0400

    Use results_file

--- collect_stats.sh
+++ collect_stats.sh
@@ -12,11 +12,9 @@ load_algo=1
 
 dataset_name=$(echo $dataset_path | awk -F/ '{gsub(/\..*$/,"",$NF); print $NF}')
 root_folder="${dataset_name}_${tag}_dir"
+results_file="${root_folder}/${dataset_name}_${tag}_results.csv"
 
 mkdir -p "${root_folder}"
-mkdir -p "${root_folder}/bulkloaded_tree_nir/"
-mkdir -p "${root_folder}/bulkloaded_tree_rstar/"
-mkdir -p "${root_folder}/bulkloaded_tree_rplus/"
 
 # generate NIR tree
 ./bin/gen_tree -t 3 -i $dataset_path -B $buffer_mem -A $load_algo -b $bulk_load_pct | tee "$root_folder/nir_load.ot"
@@ -56,52 +54,51 @@ echo "Finished running benchmarks for R* tree..."
 echo "Finished running benchmarks for R+ tree..."
 
 # generate csv file for result
-cd $root_folder
-touch output.csv
+touch $results_file
 
-echo "Search Type, NIR, R*, R+" >> output.csv
+echo "Search Type, NIR, R*, R+" >> $results_file
 
 # point search
 nir_pt=$(grep "Total page hits + misses:" nir_point_search.ot | cut -d":" -f2 | cut -c2-)
 rstar_pt=$(grep "Total page hits + misses:" rstar_point_search.ot | cut -d":" -f2 | cut -c2-)
 rplus_pt=$(grep "Total page hits + misses:" rplus_point_search.ot | cut -d":" -f2 | cut -c2-)
-echo "Point, $nir_pt, $rstar_pt, $rplus_pt" >> output.csv
+echo "Point, $nir_pt, $rstar_pt, $rplus_pt" >> $results_file
 
 # range search n=10
 nir_rec_10=$(grep "Total page hits + misses:" nir_rec_search_10.ot | cut -d":" -f2 | cut -c2-)
 rstar_rec_10=$(grep "Total page hits + misses:" rstar_rec_search_10.ot | cut -d":" -f2 | cut -c2-)
 rplus_rec_10=$(grep "Total page hits + misses:" rplus_rec_search_10.ot | cut -d":" -f2 | cut -c2-)
-echo "Range 10, $nir_rec_10, $rstar_rec_10, $rplus_rec_10" >> output.csv
+echo "Range 10, $nir_rec_10, $rstar_rec_10, $rplus_rec_10" >> $results_file
 
 # range search n=100
 nir_rec_100=$(grep "Total page hits + misses:" nir_rec_search_100.ot | cut -d":" -f2 | cut -c2-)
 rstar_rec_100=$(grep "Total page hits + misses:" rstar_rec_search_100.ot | cut -d":" -f2 | cut -c2-)
 rplus_rec_100=$(grep "Total page hits + misses:" rplus_rec_search_100.ot | cut -d":" -f2 | cut -c2-)
-echo "Range 100, $nir_rec_100, $rstar_rec_100, $rplus_rec_100" >> output.csv
+echo "Range 100, $nir_rec_100, $rstar_rec_100, $rplus_rec_100" >> $results_file
 
 # range search n=1000
 nir_rec_1000=$(grep "Total page hits + misses:" nir_rec_search_1000.ot | cut -d":" -f2 | cut -c2-)
 rstar_rec_1000=$(grep "Total page hits + misses:" rstar_rec_search_1000.ot | cut -d":" -f2 | cut -c2-)
 rplus_rec_1000=$(grep "Total page hits + misses:" rplus_rec_search_1000.ot | cut -d":" -f2 | cut -c2-)
-echo "Range 1000, $nir_rec_1000, $rstar_rec_1000, $rplus_rec_1000" >> output.csv
+echo "Range 1000, $nir_rec_1000, $rstar_rec_1000, $rplus_rec_1000" >> $results_file
 
 # range search n=10000
 nir_rec_10000=$(grep "Total page hits + misses:" nir_rec_search_10000.ot | cut -d":" -f2 | cut -c2-)
 rstar_rec_10000=$(grep "Total page hits + misses:" rstar_rec_search_10000.ot | cut -d":" -f2 | cut -c2-)
 rplus_rec_10000=$(grep "Total page hits + misses:" rplus_rec_search_10000.ot | cut -d":" -f2 | cut -c2-)
-echo "Range 10000, $nir_rec_10000, $rstar_rec_10000, $rplus_rec_10000" >> output.csv
+echo "Range 10000, $nir_rec_10000, $rstar_rec_10000, $rplus_rec_10000" >> $results_file
 
 # tree loading time
 nir_load_time=$(grep "Sequentially Inserting" nir_load.ot | cut -d":" -f2 | cut -c2-)
 rstar_load_time=$(grep "Sequentially Inserting" rstar_load.ot | cut -d":" -f2 | cut -c2-)
 rplus_load_time=$(grep "Sequentially Inserting" rplus_load.ot | cut -d":" -f2 | cut -c2-)
-echo "Tree Load Time, $nir_load_time, $rstar_load_time, $rplus_load_time" >> output.csv
+echo "Tree Load Time, $nir_load_time, $rstar_load_time, $rplus_load_time" >> $results_file
 
 # tree loading io
 nir_load_io=$(grep "Page hits" nir_load.ot | tail -n 1 | cut -d":" -f2 | cut -c2-)
 rstar_load_io=$(grep "Page hits" rstar_load.ot | tail -n 1 | cut -d":" -f2 | cut -c2-)
 rplus_load_io=$(grep "Page hits" rplus_load.ot | tail -n 1 | cut -d":" -f2 | cut -c2-)
-echo "Tree Load I/O, $nir_load_io, $rstar_load_io, $rplus_load_io" >> output.csv
+echo "Tree Load I/O, $nir_load_io, $rstar_load_io, $rplus_load_io" >> $results_file
 
 # tree sizes (KB)
 # tree sizes (MB)
@@ -114,14 +111,14 @@ rstar_tree_size_mb=$(grep "Tree Memory Usage:" rstar_load.ot | cut -d"," -f2 | c
 rplus_tree_size_kb=$(grep "Tree Memory Usage:" rplus_load.ot | cut -d":" -f2 | cut -d"K" -f1 | cut -c2-)
 rplus_tree_size_mb=$(grep "Tree Memory Usage:" rplus_load.ot | cut -d"," -f2 | cut -d"M" -f1 | cut -c2-)
 
-echo "Tree Size KB, $nir_tree_size_kb, $rstar_tree_size_kb, $rplus_tree_size_kb" >> output.csv
-echo "Tree Size MB, $nir_tree_size_mb, $rstar_tree_size_mb, $rplus_tree_size_mb" >> output.csv
+echo "Tree Size KB, $nir_tree_size_kb, $rstar_tree_size_kb, $rplus_tree_size_kb" >> $results_file
+echo "Tree Size MB, $nir_tree_size_mb, $rstar_tree_size_mb, $rplus_tree_size_mb" >> $results_file
 
 # polygon overheads (KB)
 # polygon overheads (MB)
 nir_polygon_size_kb=$(grep "Polygon with Encoding Memory Usage:" nir_load.ot | cut -d":" -f2 | cut -d"K" -f1 | cut -c2-)
 nir_polygon_size_mb=$(grep "Polygon with Encoding Memory Usage:" nir_load.ot | cut -d"," -f2 | cut -d"M" -f1 | cut -c2-)
-echo "NIR Poly Size KB, $nir_polygon_size_kb" >> output.csv
-echo "NIR Poly Size MB, $nir_polygon_size_mb" >> output.csv
+echo "NIR Poly Size KB, $nir_polygon_size_kb" >> $results_file
+echo "NIR Poly Size MB, $nir_polygon_size_mb" >> $results_file
 
-echo "finish generating $root_folder/output.csv"
+echo "Finished generating $results_file!"
