9d5f3c92387caf714a36111d1d8d15af3a39827a
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2023-11-02 14:23:14 -0400

    Add run_bench.sh

---
+++ run_bench.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Usage:
+# ./run_bench.sh 1 data/tweets.csv exp
+# Runs benchmarks
+
+tree_type="$1"
+dataset_path="$2"
+tag="$3"
+
+tree_name=''
+if [ $tree_type -eq 1 ]; then
+   tree_name='rplus'
+elif [ $tree_type -eq 2 ]; then
+   tree_name='rstar'
+elif [ $tree_type -eq 3 ]; then
+   tree_name='nir'
+else
+  echo "Unknown tree type: $tree_type"
+  exit 1
+fi
+
+buffer_mem=8000
+dataset_name=$(echo $dataset_path | awk -F/ '{gsub(/\..*$/,"",$NF); print $NF}')
+root_folder="${dataset_name}_${tag}_dir"
+
+./bin/main -t $tree_type -i $dataset_path -B $buffer_mem -S 1 \
+| tee $root_folder/${tree_name}_point_search.ot
+
+./bin/main -t $tree_type -i $dataset_path -B $buffer_mem -S 0 -R ./rects/${dataset_name}_rect_10.tst \
+| tee $root_folder/${tree_name}_rec_search_10.ot
+
+./bin/main -t $tree_type -i $dataset_path -B $buffer_mem -S 0 -R ./rects/${dataset_name}_rect_100.tst \
+| tee $root_folder/${tree_name}_rec_search_100.ot
+
+./bin/main -t $tree_type -i $dataset_path -B $buffer_mem -S 0 -R ./rects/${dataset_name}_rect_1000.tst \
+| tee $root_folder/${tree_name}_rec_search_1000.ot
+
+./bin/main -t $tree_type -i $dataset_path -B $buffer_mem -S 0 -R ./rects/${dataset_name}_rect_10000.tst \
+| tee $root_folder/${tree_name}_rec_search_10000.ot
